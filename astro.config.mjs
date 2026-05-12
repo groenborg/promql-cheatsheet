753aeca400cda9c313d7c264dd2dc7fef269dc4d
Simon Grønborg <simon.gronborg@gmail.com>
2026-05-12 23:14:11 +0200

    feat: add seo optimisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://promql.fyi",
 	integrations: [mdx()],
 	build: { inlineStylesheets: "always" },
 	vite: {
